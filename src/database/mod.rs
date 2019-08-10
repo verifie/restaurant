18dbe4ae84d0ccd3a4e86afb396d70c83037036b
Paul Emerton <paulemerton@gmail.com>
2019-08-10 11:48:54 +0100

    Handle input. Focus on strict compliance. In development. Of course, in examples so far, we've just passed the input data in raw form to MySQL.  Although we sanitize the data, this is still bad form. We must handle each entry and review for field and application compliance.

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -417,7 +417,45 @@ pub mod verifie_database_functions {
             let pool = mysql_database::Pool::new("mysql://root:d4tabasePW@localhost:3306/verifie").unwrap();
 
             // Remove additional escape /r/n characters created by the input action.
+            // TODO: Find out why these are added, and check how this is handled from web form inputs.
             let name_corrected = &name.replace("\r\n", "");
+            
+
+            // -------------------------------------------
+            // Correctly handle amount value
+            // Our objective is to only send data permitted by MySQL and the column spec.  In this case, that data is a float.
+            // The database will allow any number of characters after the decimal place, but for this application of pounds and pence,
+            // we need to ensure that two characters are sent. These rules should be explicitly implimented (as opposed to assumed outcome),
+            // even when that outcome is common sense.
+            //
+            // RULES:
+            //      1. No decimal place presented means no pence.
+            //      2. One number after a decimal place is "tens of pence". second number is "single pence".
+            //      3. Third or more numbers after the first decimal place are ignored.
+            //      4. The first decimal place is observed.  If more than one, take the first one only.  This ensures compliance and least value fail.
+            //      5. We assume the data has been sanitized and passed OK, therefore no repeat checks are required here.
+
+
+
+
+            // Find the index position of the decimal place.
+            // TODO: Check this finds the FIRST instance.
+            // TODO: What happens in the case of more than one decimal place?
+            decimal_place_index = amount.find('.');                             // Find the position of the decimal place.
+            
+            // Debug : Show where the decimal was found.
+            if DEBUG_MODE {
+                println!("\n DEBUG : Amount check: Decimal place found at = **{:?}**", decimal_place_index);
+            }
+
+
+            // We could split it and separate pounds and pence. This would be useful if MySQL had a column for each.  But it doesn't
+            // in this application, so we wont.
+            //let (amount_pounds, amount_pence) = amount.split_at(decimal_place_index);
+
+
+
+
 
             // Pull the content together in one array with supporting formatting.
             let my_sql_payee_content = ["INSERT INTO payment (amount, account_name) VALUES (", amount, ", \u{22}", name_corrected, "\u{22})"];
@@ -929,6 +967,7 @@ pub mod verifie_database_functions {
         "TRUNCATE",
         "-p",
         "-u",
+        "\u{22}",
     ];
 
 
--- src/main.rs
+++ src/main.rs
@@ -124,6 +124,7 @@ fn main() {
     database::verifie_database_functions::my_sql_payments_account(find_this);
 
 
+    // Find items greater than _amount if true, or less than if false.
     let _amount: f32 = 50.22;
     database::verifie_database_functions::my_sql_payments_due_report_value(_amount, true);
 
